58471838cdddec1a3799805f805c103142a7a61a
Glyn Davies <glyndavies@catalyst.net.nz>
2018-03-08 15:22:55 +1300

    updated functionality based on review feedback

--- fetch-installer.sh
+++ fetch-installer.sh
@@ -12,20 +12,27 @@ usage() {
   COMMAND=${0##*/}
 
   echo "
-$COMMAND [ --disable-prompts ] [ --install-dir DIRECTORY ]
+$COMMAND [ --disable-prompts ] [ --install-dir DIRECTORY ] [ --alias ALIAS_NAME ] [ --url AUTH_URL]
 
 Installs the OpenStackClient container launcher by downloading the setup script
 ($SCRIPTNAME) into a directory of your choosing, and then runs this script to
 create an alias to allow easy launching of the tools container.
 
---disable-prompts
+-d, --disable-prompts
   Disables prompts. Prompts are always disabled when there is no controlling
   tty. Alternatively export CLOUDSDK_CORE_DISABLE_PROMPTS=1 before running
   the script.
 
---install-dir=DIRECTORY
+-i, --install-dir=DIRECTORY
   Sets the installation root directory to DIRECTORY. The launcher script will be
   installed in DIRECTORY/$TOOLS_DIR. The default location is \$HOME.
+
+-a, --alias
+  The alias name to use for running openstack command via the containerised tools
+
+-u, --url
+  The authentication URL of the OpenStack Cloud that you wish to connect to.
+
 " >&2
 
   exit 2
@@ -33,7 +40,7 @@ create an alias to allow easy launching of the tools container.
 
 parseArgs() {
   # options may be followed by one colon to indicate they have a required argument
-  if ! options=$(getopt -o di: -l disable-prompt,install-dir: -- "$@")
+  if ! options=$(getopt -o di:u:a: -l disable-prompt,install-dir:,alias:,url: -- "$@")
   then
     # something went wrong, getopt will put out an error message for us
     usage
@@ -47,6 +54,8 @@ parseArgs() {
       -d|--disable-prompts) DISABLE_PROMPTS=1 ;;
       # for options with required arguments, an additional shift is required
       -i|--install-dir) eval INSTALL_DIR="$2" ; shift;;
+      -u|--url ) URL="$2"; shift;;
+      -a|--alias ) ALIAS="$2"; shift;;
       (--) shift; break;;
       (-*) echo "$0: error - unrecognized option $1" 1>&2; usage;;
       (*) break;;
@@ -109,7 +118,20 @@ fetchScript() {
 writeConfig() {
     # write out config details
     mkdir -p $CONFIG_DIR
-    echo "install-dir : $INSTALL_DIR/$TOOLS_DIR" > $CONFIG_DIR/$CONFIG_FILE
+
+    sed -i '/${INSTALL_DIR}/d' $CONFIG_DIR/$CONFIG_FILE
+    echo "install-dir = $INSTALL_DIR/$TOOLS_DIR" > $CONFIG_DIR/$CONFIG_FILE
+
+    if [ ${ALIAS} ]; then
+      sed -i '/${ALIAS}/d' $CONFIG_DIR/$CONFIG_FILE
+      printf "alias = %s\n" ${ALIAS} >> $CONFIG_DIR/$CONFIG_FILE
+    fi
+    if [ ${URL} ]; then
+      sed -i '/${URL}/d' $CONFIG_DIR/$CONFIG_FILE
+      printf "auth-url = %s\n" ${URL} >> $CONFIG_DIR/$CONFIG_FILE
+    fi
+    # strip single quotes from variables
+    sed -i "s/'//g" $CONFIG_DIR/$CONFIG_FILE
 }
 
 install() {
@@ -125,10 +147,12 @@ in the installation directory selected below,
   DESTDIR=${INSTALL_DIR}/${TOOLS_DIR}
   if [ -e $DESTDIR ]; then
     echo "$DESTDIR already exists!"
-    promptmsg="Would you like to remove the old directory?"
+    promptmsg="Would you like to remove the old directory? If you say no it will be over-written."
     while true; do
       promptYN "$promptmsg" n removedir
-      if [ $removedir == 'y' -o $removedir == 'Y' ]; then
+      if [ $removedir == 'n' -o $removedir == 'N' ]; then
+        break
+      elif [ $removedir == 'y' -o $removedir == 'Y' ]; then
         rm -rf "$DESTDIR"
         if [ ! -e "$DESTDIR" ]; then
           break
@@ -144,6 +168,7 @@ in the installation directory selected below,
   fetchScript $SCRIPT_URL $DESTDIR/$SCRIPTNAME
   chmod u+x $DESTDIR/$SCRIPTNAME || return
 
+  # output required settings to config file, to be consumed by installer
   writeConfig
 
   # run the launcher setup script
--- osclient-container-install.sh
+++ osclient-container-install.sh
@@ -57,13 +57,17 @@ check_docker_exists(){
 
 getConfig() {
   if [ -e $CONFIG_FILE ];  then
-    INSTALL_DIR=$(grep install-dir $CONFIG_FILE|awk -F ":" '{ print $2 }'| sed -e 's/^[[:space:]]//')
+    INSTALL_DIR=$(grep install-dir $CONFIG_FILE|awk -F "=" '{ print $2 }'| sed -e 's/^[[:space:]]//')
+    ALIAS=$(grep alias $CONFIG_FILE|awk -F "=" '{ print $2 }'| sed -e 's/^[[:space:]]//')
   else
     # no config file found re-run fetch-installer.sh
     echo "The config file $CONFIG_FILE could not be found, please re-run the installer"
   fi
-
+  if [ ${ALIAS} ]; then
+    ALIAS_NAME=${ALIAS}
+  fi
 }
+
 create_os_launcher(){
   if [ ! -d ${INSTALL_DIR} ]; then
     mkdir ${INSTALL_DIR}
@@ -81,6 +85,11 @@ cat << 'EOF' > ${INSTALL_DIR}/${OPENSTACK_LAUNCHER}
 # Parameters
 #------------------------------------------------------------------------------
 
+OCC_URL='https://github.com/openstack/os-client-config/archive/master.zip'
+VENDOR_FILE_PATH='os-client-config-master/os_client_config/vendors'
+TMPDIR='/tmp/oscinstall'
+TMPFILE='master.zip'
+CONFIG_FILE="$HOME/.config/openstackclient-container/cloud-container.cfg"
 OPENRCFILE="False"
 LOCALENV="False"
 MODE="interactive"
@@ -92,7 +101,7 @@ DOCKER_TAG=
 DOCKERIMAGE="catalystcloud/openstackclient-container"
 
 OS_IDENTITY_API_VERSION="3"
-OS_AUTH_URL="https://api.cloud.catalyst.net.nz:5000/"
+OS_AUTH_URL=''
 
 #------------------------------------------------------------------------------
 # Functions
@@ -134,13 +143,70 @@ handle_interruptions() {
 # under /${HOME}/${FILEPATH}. The precendence is set by this function.
 
 get_credentials() {
-  # for the osc container, you need at minimum: OS_AUTH_URL, OS_USERNAME/OS_USER_ID,
-  # OS_IDENTITY_API_VERSION, and OS_PASSWORD if not asking for it interactively.
-  if [ ${OS_USERNAME} ] && [[ ${OS_PASSWORD} || ${OS_USER_ID} ]]; then
+  # for the osc container, you need at minimum: OS_AUTH_URL, OS_USERNAME,
+  # OS_IDENTITY_API_VERSION, and OS_PASSWORD/OS_TOKEN if not asking for it interactively.
+
+  if [[ ${OS_USERNAME} && ${OS_PROJECT_NAME} && ${OS_IDENTITY_API_VERSION} ]] && [[ $OS_PASSWORD || $OS_TOKEN ]]; then
     LOCALENV="True"
   # Search for OpenStack openrc files
   elif find "${HOME}/${FILEPATH}" -name "${FILEREGEX}"; then
     OPENRCFILE="True"
+  fi
+}
+
+create_menu () {
+  arrsize=$1
+  PS3="Select the ${MENU_PROMPT} you require or type 'q' to quit: "
+  select option in "${@:2}"; do
+    if [ "$REPLY" == "q" ] || [ "$REPLY" == "Q" ] ; then
+      echo "Exiting..."
+      break;
+    elif [ 1 -le "$REPLY" ] && [ "$REPLY" -le $((arrsize)) ]; then
+      echo "You have selected :  $option"
+      break;
+    else
+      echo "Incorrect Input: Select a number 1-$arrsize"
+    fi
+  done
+}
+
+get_vendors () {
+    mkdir ${TMPDIR}
+    wget -q -O ${TMPDIR}/${TMPFILE} ${OCC_URL}
+    unzip -q ${TMPDIR}/${TMPFILE} -d ${TMPDIR}
+    arr_vendors=( $(cat ${TMPDIR}/${VENDOR_FILE_PATH}/*.json | grep '"name": ' | cut -d ':' -f2 | cut -d '"' -f2) )
+    arr_vendors+=('other')
+}
+
+get_vendor_info () {
+  if [ "$option" == "other" ]; then
+    echo "Please enter your OpenStack AUTH_URL ( in the format 'https://example.cloud.com:5000/')"
+    read OS_AUTH_URL
+  else
+    OS_AUTH_URL=$(cat ${TMPDIR}/${VENDOR_FILE_PATH}/${option}.json | grep '"auth_url": ' | cut -d '"' -f4)
+  fi
+}
+
+cleanupVendorInfo () {
+    # remove tmp dir containing
+    rm -rf ${TMPDIR}
+}
+
+getConfig() {
+  if [ -e $CONFIG_FILE ];  then
+    AUTH_URL=$(grep auth-url $CONFIG_FILE|awk -F "=" '{ print $2 }'| sed -e 's/^[[:space:]]//')
+  else
+    # no config file found re-run fetch-installer.sh
+    echo "The config file $CONFIG_FILE could not be found, please re-run the installer"
+  fi
+  if [ ${AUTH_URL} ]; then
+    OS_AUTH_URL=${AUTH_URL}
+  else
+    get_vendors
+    MENU_PROMPT='OpenStack provider'
+    create_menu "${#arr_vendors[@]}" "${arr_vendors[@]}"
+    get_vendor_info $option
+    cleanupVendorInfo
   fi
 }
 
@@ -161,24 +227,50 @@ run_container(){
   fi
 
   if [ "${OPENRCFILE}" == "True" ]; then
+    # if local openrc file/s found in $HOME/.openrc use them
     docker run -it --rm \
-    --volume ${HOME}:/home/ubuntu \
+    --security-opt=no-new-privileges \
+    --cap-drop SETUID \
+    -a stdin -a stdout -a stderr \
+    --user=$(id -u) \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -v ${HOME}:/mnt \
+    -w /mnt \
     --env "OPENRCFILE=True" \
     --hostname osclient-container ${EXTRAARGS} ${DOCKERIMAGE} ${*}
   elif [ "${LOCALENV}" == "True" ]; then
+    # if current shell has valid OS_* env variables set use them
     docker run -it --rm \
-    --volume ${HOME}:/home/ubuntu/ \
+    --security-opt=no-new-privileges \
+    --cap-drop SETUID \
+    -a stdin -a stdout -a stderr \
+    --user=$(id -u) \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -v ${HOME}:/mnt \
+    -w /mnt \
     --env "OS_PASSWORD=${OS_PASSWORD}" \
+    --env "OS_TOKEN=${OS_TOKEN}" \
     --env "OS_USERNAME=${OS_USERNAME}" \
     --env "OS_AUTH_URL=${OS_AUTH_URL}" \
+    --env "OS_AUTH_TYPE=${OS_AUTH_TYPE}" \
     --env "OS_REGION_NAME=${OS_REGION_NAME}" \
-    --env "OS_PROJECT=${OS_PROJECT_NAME}" \
+    --env "OS_PROJECT_NAME=${OS_PROJECT_NAME}" \
     --env "OS_IDENTITY_API_VERSION=${OS_IDENTITY_API_VERSION}" \
     --env "LOCALENV=True" \
     --hostname osclient-container ${EXTRAARGS} ${DOCKERIMAGE} ${*}
   else
+    # default to interactive login
     docker run -it --rm \
-    --volume ${HOME}:/home/ubuntu/ \
+    --security-opt=no-new-privileges \
+    --cap-drop SETUID \
+    -a stdin -a stdout -a stderr \
+    --user=$(id -u) \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -v ${HOME}:/mnt \
+    -w /mnt \
     --hostname osclient-container ${EXTRAARGS} ${DOCKERIMAGE} ${*}
   fi
 }
@@ -192,6 +284,8 @@ trap handle_interruptions INT
 
 parse_args "$@"
 get_credentials
+getConfig
+
 run_container ${*}
 
 EOF
@@ -203,20 +297,20 @@ chmod u+x ${INSTALL_DIR}/${OPENSTACK_LAUNCHER}
 create_alias(){
   ALIAS="alias ${ALIAS_NAME}='${INSTALL_DIR}/${OPENSTACK_LAUNCHER}'"
 
-  if [ -e ${HOME}/.bash_aliases ]; then
+  if [ -e ${HOME}/.bashrc ]; then
     if [ ${DEBUG} ]; then
-      MSG="updating .bash_aliases entry"
+      MSG="updating alias entry in .bashrc"
       echo -e  "${YELLOW}${MSG}${NC}"
     fi
-    sed -i '/${ALIAS}/d' ${HOME}/.bash_aliases
+    sed -i '/${ALIAS}/d' ${HOME}/.bashrc
     # make sure to append so as to not clobber existing alias entries
-    echo "${ALIAS}" >> "${HOME}"/.bash_aliases
+    echo "${ALIAS}" >> "${HOME}"/.bashrc
   else
     if [ ${DEBUG} ]; then
-      MSG="creating .bash_aliases entry"
+      MSG="creating alias entry in .bashrc"
       echo -e  "${YELLOW}${MSG}${NC}"
     fi
-    echo ${ALIAS} > ${HOME}/.bash_aliases
+    echo ${ALIAS} > ${HOME}/.bashrc
   fi
 }
 
@@ -234,15 +328,15 @@ update_path(){
   if [ ${NEWPATH} ]; then
     MSG="
     Please run 'source ~/.bashrc' to enable changes to \$PATH
-    The alias '${ALIAS}' was added to your .bash_aliases file.
+    The alias '${ALIAS}' was added to your .bashrc file.
     "
     echo -e  "${GREEN}${MSG}${NC}"
   fi
 
   if [ ! ${NEWPATH} ]; then
     MSG="
-    The alias '${ALIAS}' was added to your .bash_aliases file.
-    Please run 'source ${HOME}/.bash_aliases' to make this available.
+    The alias '${ALIAS}' was added to your .bashrc file.
+    Please run 'source ${HOME}/.bashrc' to make this available.
     "
 
     echo -e  "${GREEN}${MSG}${NC}"
